add122bd4c7f6ca9effaf2080582f8cb44e457b8
lifinux <lifinux@gmail.com>
2022-02-22 22:44:04 +0700

    Init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+    </el-row>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+    </el-row>
+  </div>
+</template>
